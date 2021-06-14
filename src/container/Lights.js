cdd07d2b7e380764fcf7f4eb620ac9b4f7499d05
yescine <bouchoucha.yassine@gmail.com>
2021-06-14 14:40:25 +0100

    add Light helpers

--- src/container/Lights.js
+++ src/container/Lights.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import {RectAreaLightHelper} from 'three/examples/jsm/helpers/RectAreaLightHelper'
 import {GUI} from 'dat.gui'
 
 const Group = new THREE.Group()
@@ -15,10 +16,10 @@ plane.rotation.x=-Math.PI/2
 
 
 const sphere = new THREE.Mesh(
-   new THREE.SphereBufferGeometry(0.4,16,16),
+   new THREE.SphereBufferGeometry(0.3,16,16),
    simpleMaterial
 )
-sphere.position.set(0,1,0)
+sphere.position.set(0,0.4,0)
 
 const cube = new THREE.Mesh(
    new THREE.BoxBufferGeometry(0.4,0.4,0.4),
@@ -32,23 +33,42 @@ const torus = new THREE.Mesh(
 )
 torus.position.set(1.2,1,0)
 
+const helperSize = 0.2
 // light 
 const ambientLight = new THREE.AmbientLight('#ffffff',0.1)
+
 const pointLight = new THREE.PointLight('#ffffff',0.6,7,1);pointLight.position.set(2,3,4)
+const pointLightHelper = new THREE.PointLightHelper(pointLight,helperSize)
+
 const directionalLight = new THREE.DirectionalLight('#ba68c6',0.4);directionalLight.position.set(0,0,-4)
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight,helperSize)
+
 const hemisphereLight = new THREE.HemisphereLight('#3949ab','#ffab40',0.7)
-const rectLight = new THREE.RectAreaLight(0x4e00ff,3,2,2);
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight,helperSize)
+
+const rectLight = new THREE.RectAreaLight(0x4e00ff,3,1.5,1.5);
+const reactLightHelper = new RectAreaLightHelper(rectLight)
 rectLight.position.set(0,0,2);rectLight.lookAt(torus.position);
+
+
 const spotLigth = new THREE.SpotLight('#78ff00',0.5,5,Math.PI*0.1,0.1,1)
+const spotLigthHelper = new THREE.SpotLightHelper(spotLigth,helperSize)
 spotLigth.position.set(0,2,3);spotLigth.target.position.set(-0.75,0,0)
+
+window.requestAnimationFrame(()=>{
+   spotLigthHelper.update()
+   reactLightHelper.position.copy(rectLight.position)
+   reactLightHelper.quaternion.copy(rectLight.quaternion)
+
+ })
 
 Group.add(
    ambientLight, 
-   pointLight,
-   directionalLight,
-   hemisphereLight,
-   rectLight,
-   spotLigth,spotLigth.target
+   pointLight,pointLightHelper,
+   directionalLight,directionalLightHelper,
+   hemisphereLight,hemisphereLightHelper,
+   rectLight,reactLightHelper,
+   spotLigth,spotLigth.target,spotLigthHelper
 )
 
 Group.add(plane)
@@ -76,6 +96,7 @@ const tick = ()=>{
    cube.rotation.x = 0.1*elapsedTime
    sphere.rotation.x = 0.1*elapsedTime
 
+   spotLigthHelper.update()
    window.requestAnimationFrame(tick)
 
 }
