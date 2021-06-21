28c105fe6ca827d79256a695369305e68fbd9ee8
yescine <bouchoucha.yassine@gmail.com>
2021-06-21 13:31:50 +0100

    add glsl webpack plugin

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -82,7 +82,16 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            // Shaders
+            {
+                test:/\.(glsl|vs|fs|vert|frag)$/,
+                exclude:/node_modules/,
+                use:[
+                    'raw-loader'
+                ]
             }
+
         ]
     }
 }
