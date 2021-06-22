b73e3cec18748ee19d115be5ecbea1352169519d
yescine <bouchoucha.yassine@gmail.com>
2021-06-22 05:01:59 +0100

    add shader pattern

---
+++ src/container/Shaders/Template/vertexTemplate.glsl
@@ -0,0 +1,24 @@
+// uniform mat4 projectionMatrix;
+// uniform mat4 viewMatrix;
+// uniform mat4 modelMatrix;
+// attribute vec2 uv;
+// attribute vec3 position;
+
+
+// varying vec2 vUV;
+
+void main(){
+
+   // create plane
+   vec4 modelPosition = modelMatrix * vec4(position,1.0);
+   modelPosition.y += -0.10;
+
+   vec4 viewPosition = viewMatrix * modelPosition;
+   vec4 projectionPosition = projectionMatrix *viewPosition;
+
+   gl_Position = projectionPosition;
+
+   // send data & Attribute to fragment
+   // vUV = uv;
+
+}
